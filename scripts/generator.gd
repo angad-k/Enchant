4be71ae74017f22ed0c14e5f70ddb902ce8bf3d9
Angad Kambli <angadkambli@gmail.com>
2021-12-27 05:27:42 +0530

    use threads for instancing and move to GodotPhysics

--- scripts/generator.gd
+++ scripts/generator.gd
@@ -4,21 +4,25 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var test_set1 = preload("res://set_scenes/forest_set.tscn")
-var test_set2 = preload("res://set_scenes/swing_set.tscn")
+var thread
+const test_set1 = preload("res://set_scenes/forest_set.tscn")
+const test_set2 = preload("res://set_scenes/swing_set.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func trigger_generation(position):
+func trigger_generation(p_position):
+	thread = Thread.new()
+	thread.start(self, "load_set_scene", p_position)
+
+func load_set_scene(p_position):
 	var x
 	if(randf()<0.5):
 		x = test_set1.instance()
 	else:
 		x = test_set2.instance()
-	x.position = position
+	x.position = p_position
 	get_tree().root.add_child(x)
-	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
