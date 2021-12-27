a209b41e939e839521ed900a7d88c03084dc9a1d
mayankmittal-iitr <mmittal123456@gmail.com>
2021-12-27 22:19:21 +0530

    forest set script(todo)

--- set_scenes/forest_set.gd
+++ set_scenes/forest_set.gd
@@ -8,6 +8,10 @@ extends Node2D
 var stump = preload("res://utility_scenes/obstacles/stump.tscn")
 var thorns = preload("res://utility_scenes/obstacles/thorns.tscn")
 var logs = preload("res://utility_scenes/obstacles/logs.tscn")
+var tree1 = preload("res://utility_scenes/trees/tree1.tscn")
+var tree2 = preload("res://utility_scenes/trees/tree2.tscn")
+var tree3 = preload("res://utility_scenes/trees/tree3.tscn")
+var tree4 = preload("res://utility_scenes/trees/tree4.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for sp in $obstacle_spawn_points.get_children():
@@ -25,6 +29,28 @@ func _ready():
 				var s = logs.instance()
 				s.position = sp.position
 				$obstacle_spawn_points.add_child(s)
+				
+	for node in $tree_spawn_points.get_childen():
+		if(randf() < 0.6):
+			var randval = randf()
+			if(randval < 0.25):
+				var s = tree1.instance()
+				s.position = node.position
+				$tree_spawn_points.add_child(s)
+			elif(randval < 0.5):
+				var s = tree2.instance()
+				s.position = node.position
+				$tree_spawn_points.add_child(s)
+			elif(randval < 0.75):
+				var s = tree3.instance()
+				s.position = node.position
+				$tree_spawn_points.add_child(s)
+			else:
+				var s = tree4.instance()
+				s.position = node.position
+				$tree_spawn_points.add_child(s)
+				
+			
 	pass # Replace with function body.
 
 
